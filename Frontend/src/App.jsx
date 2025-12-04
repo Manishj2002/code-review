704954df7f7bd4583e8406497a38f0984330a4d6
manish jakhar <manish946050@gmail.com>
2025-12-04 20:10:10 +0530

    Updated API URL

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -9,7 +9,6 @@ import axios from 'axios'
 import './App.css'
 
 function App() {
-  const [ count, setCount ] = useState(0)
   const [ code, setCode ] = useState(` function sum() {
   return 1 + 1
 }`)
@@ -21,7 +20,7 @@ function App() {
   }, [])
 
   async function reviewCode() {
-    const response = await axios.post('http://localhost:3000/ai/get-review', { code })
+    const response = await axios.post('https://code-review-backend-7feu.onrender.com/ai/get-review', { code })
     setReview(response.data)
   }
 
